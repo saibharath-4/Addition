6f0ec84bd13b32f13a26b410cf8c4a3087352ec9
Arunkanth Goud Alwala <arunkanth.a@talentsprint.com>
2016-06-16 18:55:37 +0530

    Added logic

--- Addition/src/SumOfTwoIntegers.java
+++ Addition/src/SumOfTwoIntegers.java
@@ -5,12 +5,13 @@ public class SumOfTwoIntegers {
 		// TODO Auto-generated method stub
 		int a=10;
 		int b=20;
-		int c=a/b;
+		int c=a+b;
+		int d=a%b;
 		System.out.println(c);
 		System.out.println("Result is "+c);
 		System.out.println("C");
 		System.out.println();
-	
+		System.out.print(d);
 		
 
 	}
