c817bb2a8a3c0e6c3252a1861231cb84a99ee384
saibharath-4 <saibharath.dachepalli4@gmail.com>
2016-06-16 18:52:08 +0530

    Added multiplication logic

--- Addition/src/SumOfTwoIntegers.java
+++ Addition/src/SumOfTwoIntegers.java
@@ -6,11 +6,13 @@ public class SumOfTwoIntegers {
 		int a=10;
 		int b=20;
 		int c=a/b;
+		int d=a*b;
 		System.out.println(c);
 		System.out.println("Result is "+c);
 		System.out.println("C");
 		System.out.println();
-	
+		System.out.print("d");
+		
 		
 
 	}
